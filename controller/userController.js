da7c6aa026f437e0b0c86324fbd8e497c3646d1e
Darshan Gowda <iamdarshangowda@gmail.com>
2023-04-15 13:39:54 +0530

    register and login user controller updated

--- controller/userController.js
+++ controller/userController.js
@@ -1,11 +1,68 @@
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcrypt");
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.json({ message: "Register the user" });
+  const { username, password, email } = req.body;
+
+  if (!username || !password || !email) {
+    res.status(400);
+    throw new Error("All feilds are mandatory");
+  }
+
+  const userAvailable = await User.findOne({ email });
+
+  if (userAvailable) {
+    res.status(400);
+    throw new Error("User already registered");
+  }
+
+  //Hash Password
+  const hashPassword = await bcrypt.hash(password, 10);
+  console.log("hash", hashPassword);
+
+  const user = await User.create({
+    username,
+    password: hashPassword,
+    email,
+  });
+
+  if (user) {
+    res.status(201).json(user);
+  } else {
+    res.status(400);
+    throw new Error("User data not valid");
+  }
 });
 
 const loginUser = asyncHandler(async (req, res) => {
-  res.json({ message: "Login the user" });
+  const { email, password } = req.body;
+
+  if (!password || !email) {
+    res.status(400);
+    throw new Error("All feilds are mandatory");
+  }
+
+  const user = await User.findOne({ email });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    const accessToken = jwt.sign(
+      {
+        user: {
+          username: user.username,
+          email: user.email,
+          id: user.id,
+        },
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "1m" }
+    );
+    res.json({ accessToken });
+  } else {
+    res.status(401);
+    throw new Error("Email or Password is not valid");
+  }
 });
 
 const currentUser = asyncHandler(async (req, res) => {
